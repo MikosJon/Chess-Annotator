2f87cd0b07fe2e0e90fa5783ce854f81651b9c57
Jon Mikoš <atletjm@gmail.com>
2020-08-20 19:38:00 +0200

    dodano shranjevanje PGN in potez

--- __main__.py
+++ __main__.py
@@ -68,12 +68,12 @@ def login():
 
 @bottle.post('/login')
 def login():
-    username = bottle.request.forms.flogin_username
+    username = bottle.request.forms.login_username
     user = USERS.get(username, None)
     if user is None:
         bottle.redirect('/login')
 
-    password = bottle.request.forms.fpassword
+    password = bottle.request.forms.password
     key = hashlib.pbkdf2_hmac(
         'sha256',
         password.encode('utf-8'),
@@ -90,11 +90,11 @@ def login():
 
 @bottle.post('/register')
 def register():
-    username = bottle.request.forms.fregister_username
+    username = bottle.request.forms.register_username
     if username in USERS:
         bottle.redirect('/login')
-    password1 = bottle.request.forms.fpassword1
-    password2 = bottle.request.forms.fpassword2
+    password1 = bottle.request.forms.password1
+    password2 = bottle.request.forms.password2
 
     if password1 != password2:
         bottle.redirect('/login')
@@ -121,20 +121,14 @@ def logout():
 @bottle.get('/analysis')
 def analysis():
     user = get_current_user()
-    args = {}
-    args['game'] = user.game
-    args['moves'] = user.moves
-    args['move_num'] = user.current_move_number
-    args['last_color'] = user.last_played
-    args['username'] = user.username
-    return bottle.template('analysis.html', args=args, login=False)
+    return bottle.template('analysis.html', user=user, login=False)
 
 @bottle.post('/make_move')
 def make_move():
     user = get_current_user()
-    notation = bottle.request.forms.fmove
-    annotation = bottle.request.forms.fanno
-    text = bottle.request.forms.ftext
+    notation = bottle.request.forms.move
+    annotation = bottle.request.forms.anno
+    text = bottle.request.forms.text
     try:
         user.game.make_move_from_notation(notation)
     except ValueError as err:
@@ -152,8 +146,8 @@ def make_move():
 @bottle.post('/update_move')
 def update_move():
     user = get_current_user()
-    anno = bottle.request.forms.fanno
-    text = bottle.request.forms.ftext
+    anno = bottle.request.forms.anno
+    text = bottle.request.forms.text
 
     idx = user.next_move_idx()
     move = user.moves[idx]
@@ -211,11 +205,14 @@ def to_last():
 def remove_from_now():
     user = get_current_user()
     user.moves = user.moves[:user.next_move_idx()]
+    user.game_end = user.game.game_state
     bottle.redirect('/')
 
-@bottle.post('/export_pgn')
-def export_pgn():
+@bottle.post('/save_moves')
+def save_moves():
     user = get_current_user()
+    result = bottle.request.forms.result
+
     with open(os.path.join(USERS_DIR, user.username, 'current.pgn'), 'w') as f:
         for idx, (move, notation_info, anno, text) in enumerate(user.moves):
             alg_notation = to_algebraic_notation(move, notation_info)
@@ -229,14 +226,47 @@ def export_pgn():
             if text:
                 f.write(f' {{{text}}}')
 
-        if user.game_end == GameState.Draw:
-            f.write(' 1/2-1/2')
-        elif user.game_end == GameState.White:
-            f.write(' 1-0')
-        elif user.game_end == GameState.Black:
-            f.write(' 0-1')
-        else:
-            f.write(' *')
+        f.write(' ' + result)
+    return bottle.static_file('current.pgn', root=os.path.join(USERS_DIR, user.username), download=True)
+
+@bottle.post('/export_pgn')
+def export_pgn():
+    user = get_current_user()
+
+    event = bottle.request.forms.event
+    city = bottle.request.forms.city
+    region = bottle.request.forms.region
+    country = PGN_COUNTRY_CODES[bottle.request.forms.country]
+    date = bottle.request.forms.date
+    event_round = bottle.request.forms.event_round
+    white_name = bottle.request.forms.white_name
+    white_surname = bottle.request.forms.white_surname
+    black_name = bottle.request.forms.black_name
+    black_surname = bottle.request.forms.black_surname
+    result = bottle.request.forms.result
+
+    with open(os.path.join(USERS_DIR, user.username, 'current.pgn'), 'w') as f:
+        f.write(f'[Event "{event}"]\n')
+        f.write(f'[Site "{city}, {region} {country}"]\n')
+        f.write(f'[Date "{date}"]\n')
+        f.write(f'[Round "{event_round}"]\n')
+        f.write(f'[White "{white_surname}, {white_name}"]\n')
+        f.write(f'[Black "{black_surname}, {black_name}"]\n')
+        f.write(f'[Result "{result}"]\n\n')
+
+        for idx, (move, notation_info, anno, text) in enumerate(user.moves):
+            alg_notation = to_algebraic_notation(move, notation_info)
+            if idx == 0:
+                f.write('1.')
+            elif move.color == Color.White:
+                f.write(' ' + str(idx // 2 + 1) + '.')
+            f.write(f' {alg_notation}')
+            if anno != '0':
+                f.write(f'${anno}')
+            if text:
+                f.write(f' {{{text}}}')
+        if result != '*':
+            f.write(f' {result}')
     return bottle.static_file('current.pgn', root=os.path.join(USERS_DIR, user.username), download=True)
 
 bottle.run(debug=True, reloader=True)
